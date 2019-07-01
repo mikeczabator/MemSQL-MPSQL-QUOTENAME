26a8f6973852e97a1caed3e188c18c8a4073814b
mikeczabator <35580719+mikeczabator@users.noreply.github.com>
2019-07-01 11:23:59 -0400

    Update QUOTENAME.sql
    added optional 2nd input param to define delimiter. default is still '`'

--- QUOTENAME.sql
+++ QUOTENAME.sql
@@ -1,12 +1,12 @@
 delimiter // 
-CREATE FUNCTION QUOTENAME(str varchar(64)) 
+CREATE FUNCTION QUOTENAME(_in_str varchar(64), _in_delimiter char(1) default '`') 
 returns varchar(66) as 
 DECLARE 
   retval varchar(66); 
   delimiter char(1);
 BEGIN
   delimiter='`';
-  retval=CONCAT(delimiter, REPLACE(str, delimiter, CONCAT(delimiter,delimiter)), delimiter);
+  retval=CONCAT(_in_delimiter, REPLACE(_in_str, _in_delimiter, CONCAT(_in_delimiter,_in_delimiter)), _in_delimiter);
   return (retval);
 end;
 // 
